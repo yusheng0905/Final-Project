8799e57b32ce8cf23343a3d202cd3c3efd9b4fc1
yusheng lin <jacky053696@gmail.com>
2023-06-02 22:56:53 +0800

    home initialized

---
+++ app/src/main/java/com/ncueel/finalproject/MainActivity_Home.kt
@@ -0,0 +1,11 @@
+package com.ncueel.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_home)
+    }
+}
