44339d47bf7998c8b2bfe026a004af945be54277
yusheng lin <jacky053696@gmail.com>
2023-06-03 01:01:55 +0800

    home bottom navigaion

--- app/src/main/java/com/ncueel/finalproject/MainActivity_Home.kt
+++ app/src/main/java/com/ncueel/finalproject/MainActivity_Home.kt
@@ -1,11 +1,24 @@
 package com.ncueel.finalproject
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.ncueel.finalproject.databinding.ActivityMainHomeBinding
 
 class MainActivity_Home : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainHomeBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_home)
+
+        binding = ActivityMainHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = Navigation.findNavController(this, R.id.activity_home_nav_fragment)
+        setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
