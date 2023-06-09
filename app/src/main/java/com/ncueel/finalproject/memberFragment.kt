ad292eb6a0626c7d62d7a502b485981fc304f7a2
yusheng lin <jacky053696@gmail.com>
2023-06-10 02:34:43 +0800

    logout finished

--- app/src/main/java/com/ncueel/finalproject/memberFragment.kt
+++ app/src/main/java/com/ncueel/finalproject/memberFragment.kt
@@ -6,19 +6,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import com.ncueel.finalproject.databinding.FragmentMemberBinding
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [memberFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
 class memberFragment : Fragment() {
     //Step 1: 初始化FirebaseAuth
     private lateinit var auth: FirebaseAuth
 
-    //Step 1:先從註冊這個功能開始寫->先初始化一個資料鏈結的部分
-    private lateinit var binding: FragmentMemberBinding
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,8 +49,31 @@ class memberFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //Step 4: 設計logout button功能
-        binding.button3.setOnClickListener {
+        val logout = view.findViewById<Button>(R.id.button3)
+        logout.setOnClickListener {
             auth.signOut()
+            val intent = Intent(requireContext(),MainActivity::class.java)
+            startActivity(intent)
         }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment memberFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            memberFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
     }
 }
