1efbc720f39f10d2f27d5030088541a3e36d8756
yusheng lin <jacky053696@gmail.com>
2023-06-10 00:29:03 +0800

    firebase connect

--- app/src/main/java/com/ncueel/finalproject/MainActivity.kt
+++ app/src/main/java/com/ncueel/finalproject/MainActivity.kt
@@ -3,17 +3,95 @@ package com.ncueel.finalproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.FirebaseUser
+import com.ncueel.finalproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //Step 1: 初始化FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+
+    //Step 1:先從註冊這個功能開始寫->先初始化一個資料鏈結的部分
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //** Step 2: 去設定我們要去啟動的RegisterActivity **
+        //Step 2-1: 初始化FirebaseAuth->初始化auth
+        auth = Firebase.auth
+
+        //Step 2-2:先從註冊這個功能開始寫->初始化binding要鏈結的layout
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         //跳到註冊頁面
-        val signPage = findViewById<Button>(R.id.button2)
-        signPage.setOnClickListener{
+        binding.button2.setOnClickListener{
             startActivity(Intent(this,MainActivity2::class.java))
         }
+
+        //Step 3-1: 設計登入的button
+        binding.button.setOnClickListener {
+            if (binding.editTextText.text.toString().isEmpty()) {
+                showMessage("請輸入帳號")
+            } else if (binding.editTextTextPassword.text.toString().isEmpty()) {
+                showMessage("請輸入密碼")
+            } else {
+                signIn()
+            }
+        }
+    }
+    //Step 3-2: 設計登入的button->寫一個登入認證失敗的判斷式
+    private fun signIn() {
+        val email = binding.editTextText.text.toString()
+        val password = binding.editTextTextPassword.text.toString()
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    //Log.d("signInWithEmail:success")
+                    println("---------signInWithEmail:success-----------")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    it.exception?.message?.let {  }
+                    println("---------error---------------")
+                    showMessage("登入失敗，帳號或密碼錯誤")
+                    updateUI(null)
+                }
+            }
+        startActivity(Intent(this,MainActivity_Home::class.java))
+    }
+
+    //Step 6: 確認更新登入的user狀況
+    private fun updateUI(user: FirebaseUser?) {
+        if ( user!= null){
+            //已登入
+            binding.editTextText.visibility = View.GONE
+            binding.editTextTextPassword.visibility = View.GONE
+            binding.button.visibility = View.GONE
+            binding.button2.visibility = View.GONE
+
+
+        }else{
+            //未登入
+            binding.editTextText.visibility = View.VISIBLE
+            binding.editTextTextPassword.visibility = View.VISIBLE
+            binding.button.visibility = View.VISIBLE
+            binding.button2.visibility = View.VISIBLE
+
+        }
+    }
+
+    //Step 3-3: 設計登入的button->設計一個給使用者確認的message
+    private fun showMessage(message: String) {
+        val alertDialog: AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
+        alertDialog.setMessage(message)
+        alertDialog.setPositiveButton("確定") { dialog, which -> }
+        alertDialog.show()
     }
 }
--- app/src/main/java/com/ncueel/finalproject/MainActivity2.kt
+++ app/src/main/java/com/ncueel/finalproject/MainActivity2.kt
@@ -2,25 +2,51 @@ package com.ncueel.finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.ncueel.finalproject.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+    // Step 1: 先初始化 binding的變數與FirebaseAuth變數
+    private lateinit var binding: ActivityMain2Binding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        //清除重填
-        val button = findViewById<Button>(R.id.button3)
-        val nameEditText = findViewById<EditText>(R.id.name)
-        button.setOnClickListener {nameEditText.setText("")}
-//        val personIDEditText = findViewById<EditText>(R.id.personID)
-//        button.setOnClickListener {personIDEditText.setText("")}
-//        val phoneEditText = findViewById<EditText>(R.id.phone)
-//        button.setOnClickListener {phoneEditText.setText("")}
-//        val emailEditText = findViewById<EditText>(R.id.email)
-//        button.setOnClickListener {emailEditText.setText("")}
-//        val usernameEditText = findViewById<EditText>(R.id.username)
-//        button.setOnClickListener {usernameEditText.setText("")}
+        auth = Firebase.auth
+
+        //Step 2: 點下signUp後需要取得editview的email與password兩個欄位的值
+        binding.button4.setOnClickListener {
+
+            //step 2: 取得email and password
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+
+            //step 3: auth內有一個create user using email and password的function
+            auth.createUserWithEmailAndPassword(email,password) //把變數email password放進去
+                .addOnCompleteListener {
+                    //Step 3:在這裡去判斷傳入的值是否有成功，所以要寫一個判斷式
+                    if (it.isSuccessful){
+                        Log.d("Test","成功註冊")
+                        finish()//註冊成功就關閉這個頁面
+                    }else{
+                        Log.w("Test","註冊失敗", it.exception) //it.exception是把錯誤原因記下來
+                        showMessage("註冊會員失敗")//去呼叫showMessage來顯示註冊失敗
+                    }
+                }
+        }
+    }
+    private fun showMessage(message: String) {
+        val alertDialog: AlertDialog.Builder = AlertDialog.Builder(this)
+        alertDialog.setMessage(message)
+        alertDialog.setPositiveButton("確定") { dialog, which -> }
+        alertDialog.show()
     }
 }
