cb6eff41cff1280c51a08fdb223fb3365052b364
yusheng lin <jacky053696@gmail.com>
2023-06-12 01:15:31 +0800

    icon change

--- app/src/main/java/com/ncueel/finalproject/homeFragment.kt
+++ app/src/main/java/com/ncueel/finalproject/homeFragment.kt
@@ -8,9 +8,13 @@ import android.view.ViewGroup
 import android.widget.GridView
 import android.widget.SimpleAdapter
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.denzcoskun.imageslider.ImageSlider
 import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.models.SlideModel
+import com.google.firebase.firestore.AggregateSource
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +27,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class homeFragment : Fragment() {
+    private var db = Firebase.firestore
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -59,11 +64,66 @@ class homeFragment : Fragment() {
         imageSlider.setImageList(imageList, ScaleTypes.CENTER_INSIDE)
 
         //gridView商品
-        val goodsNames = arrayOf("Itsukushima", "Kyoto", "Mount Fuji", "Okinawa", "Sensoji Temple")
+        /*連接資料庫讀取商品
+        val gridView = view.findViewById<GridView>(R.id.gridView)
+        val grid = ArrayList<HashMap<String, Any>>()
+        val goodsQuery = db.collection("goods")
+        val goodsCountQuery = goodsQuery.count()
+        goodsCountQuery.get(AggregateSource.SERVER).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                // Count fetched successfully
+                val snapshot = task.result
+                val goodsCount = snapshot.count
+                for(i in 0..goodsCount) {
+                    val map = HashMap<String, Any>()
+                    val goodDoc = goodsQuery.document("0001")
+                    goodDoc.get().addOnSuccessListener {
+                        if (it != null) {
+                            val name = it.data?.get("name").toString()
+                            val price = it.data?.get("price").toString()
+                            val number = it.data?.get("number").toString()
+                            val pictureUrl = it.data?.get("pictureUrl").toString()
 
-        val goodsPrices = arrayOf("50", "51", "52", "53", "54")
+                            map["goodsName"] = name
+                            map["goodsPrice"] = "$${price}"
+                            map["goodsImage"] = pictureUrl
+                            map["number"] = number
+                            grid.add(map)
+                        }
+                    }
+                        .addOnFailureListener {
+                            Toast.makeText(requireContext(), "Failed", Toast.LENGTH_SHORT).show()
+                        }
 
-        val goodsImageIds = arrayOf(R.drawable.itsukushima, R.drawable.kyoto, R.drawable.mount_fuji, R.drawable.okinawa, R.drawable.sensoji_temple)
+                }
+                val fromData = arrayOf("goodsName", "goodsPrice", "goodsImage")
+                val toData = intArrayOf(R.id.textView6, R.id.textView10, R.id.imageView)
+                val simpleAdapter = SimpleAdapter(requireContext(), grid, R.layout.grid_row_items_home, fromData, toData)
+                gridView.adapter = simpleAdapter
+                gridView.setOnItemClickListener { _, _, i, _ ->
+                    Toast.makeText(context, "${fromData[i]}",Toast.LENGTH_SHORT).show()
+                }
+
+            } else {
+                Toast.makeText(requireContext(), "Count Failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+         */
+
+        val goodsNames = arrayOf("Nintendo Switch（OLED款式）白色",
+            "PlayStation 5 遊戲主機",
+            "PlayStation 5 Digital Edition遊戲主機",
+            "Xbox Series X《極限競速-地平線5》同捆組",
+            "Xbox Series S - Game Pass 超值組")
+
+        val goodsPrices = arrayOf("10280", "17580", "14580", "16999", "9980")
+
+        val goodsImageIds = arrayOf(R.drawable.nintendo_switch_white,
+            R.drawable.ps5_disk,
+            R.drawable.ps5_digital,
+            R.drawable.xboxseriesx,
+            R.drawable.xboxseriess)
 
         val gridView = view.findViewById<GridView>(R.id.gridView)
         val grid = ArrayList<HashMap<String, Any>>()
