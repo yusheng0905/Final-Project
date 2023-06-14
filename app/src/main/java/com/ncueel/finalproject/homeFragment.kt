680d6c4973aacebe28b517100634da4dfeecca56
yusheng lin <jacky053696@gmail.com>
2023-06-15 00:27:13 +0800

    product info create

--- app/src/main/java/com/ncueel/finalproject/homeFragment.kt
+++ app/src/main/java/com/ncueel/finalproject/homeFragment.kt
@@ -7,14 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.GridView
 import android.widget.SimpleAdapter
+import android.widget.TextView
 import android.widget.Toast
-import com.bumptech.glide.Glide
 import com.denzcoskun.imageslider.ImageSlider
 import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.models.SlideModel
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.firestore.AggregateSource
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -64,9 +67,14 @@ class homeFragment : Fragment() {
         imageSlider.setImageList(imageList, ScaleTypes.CENTER_INSIDE)
 
         //gridView商品
-        /*連接資料庫讀取商品
+        //連接資料庫讀取商品
+        val goodsNames = ArrayList<String>()
+        val goodsPrices = ArrayList<String>()
+        val goodsNumbers = ArrayList<String>()
+        val goodsImageIds = ArrayList<String>()
+        val goodsPIds = ArrayList<String>()
         val gridView = view.findViewById<GridView>(R.id.gridView)
-        val grid = ArrayList<HashMap<String, Any>>()
+        //val grid = ArrayList<HashMap<String, Any>>()
         val goodsQuery = db.collection("goods")
         val goodsCountQuery = goodsQuery.count()
         goodsCountQuery.get(AggregateSource.SERVER).addOnCompleteListener { task ->
@@ -74,43 +82,64 @@ class homeFragment : Fragment() {
                 // Count fetched successfully
                 val snapshot = task.result
                 val goodsCount = snapshot.count
-                for(i in 0..goodsCount) {
-                    val map = HashMap<String, Any>()
-                    val goodDoc = goodsQuery.document("0001")
-                    goodDoc.get().addOnSuccessListener {
-                        if (it != null) {
-                            val name = it.data?.get("name").toString()
-                            val price = it.data?.get("price").toString()
-                            val number = it.data?.get("number").toString()
-                            val pictureUrl = it.data?.get("pictureUrl").toString()
 
-                            map["goodsName"] = name
-                            map["goodsPrice"] = "$${price}"
-                            map["goodsImage"] = pictureUrl
-                            map["number"] = number
-                            grid.add(map)
-                        }
+                val map = HashMap<String, Any>()
+                //val goodDoc = goodsQuery.document("0001")
+                goodsQuery.get().addOnSuccessListener {
+                    for(goodsDoc in it) {
+                        val name = goodsDoc?.get("name").toString()
+                        val price = goodsDoc?.get("price").toString()
+                        val number = goodsDoc?.get("number").toString()
+                        val pictureUrl = goodsDoc?.get("pictureUrl").toString()
+                        val productId = goodsDoc?.id.toString()
+
+                        goodsNames.add(name)
+                        goodsPrices.add("$${price}")
+                        goodsNumbers.add(number)
+                        goodsImageIds.add(pictureUrl)
+                        goodsPIds.add(productId)
                     }
-                        .addOnFailureListener {
-                            Toast.makeText(requireContext(), "Failed", Toast.LENGTH_SHORT).show()
-                        }
+                    //view.findViewById<TextView>(R.id.textView2).text = goodsNames.toString()
+                    /*
+                    for(i in goodsNames.indices) {
+                        val map = HashMap<String, Any>()
+                        map["goodsName"] = goodsNames[i]
+                        map["goodsPrice"] = "$${goodsPrices[i]}"
+                        map["goodsImage"] = goodsImageIds[i]
+                        map["productId"] = goodsPIds[i]
+                        grid.add(map)
+                    }
+                    val fromData = arrayOf("goodsName", "goodsPrice", "goodsImage")
+                    val toData = intArrayOf(R.id.textView6, R.id.textView10, R.id.imageView)
 
+                    */
+                    val customAdapter = CustomAdapter(requireContext(), goodsNames, goodsPrices, goodsImageIds)
+                    gridView.adapter = customAdapter
+                    gridView.setOnItemClickListener { _, _, i, _ ->
+                        //Toast.makeText(context, "${goodsPIds[i]}",Toast.LENGTH_SHORT).show()
+                        val dialogView = layoutInflater.inflate(R.layout.bottom_sheet, null)
+                        val dialog = BottomSheetDialog(requireContext())
+
+                        dialog.setContentView(dialogView)
+                        Picasso.get().load(goodsImageIds[i]).into(dialog.findViewById(R.id.imageView2))
+                        dialog.findViewById<TextView>(R.id.textView12)?.text = goodsNames[i]
+                        dialog.findViewById<TextView>(R.id.textView13)?.text = goodsPrices[i]
+                        dialog.findViewById<TextView>(R.id.textView15)?.text = goodsNumbers[i]
+                        dialog.show()
+                    }
                 }
-                val fromData = arrayOf("goodsName", "goodsPrice", "goodsImage")
-                val toData = intArrayOf(R.id.textView6, R.id.textView10, R.id.imageView)
-                val simpleAdapter = SimpleAdapter(requireContext(), grid, R.layout.grid_row_items_home, fromData, toData)
-                gridView.adapter = simpleAdapter
-                gridView.setOnItemClickListener { _, _, i, _ ->
-                    Toast.makeText(context, "${fromData[i]}",Toast.LENGTH_SHORT).show()
-                }
+                    .addOnFailureListener {
+                        Toast.makeText(requireContext(), "Failed", Toast.LENGTH_SHORT).show()
+                    }
+
+
 
             } else {
                 Toast.makeText(requireContext(), "Count Failed", Toast.LENGTH_SHORT).show()
             }
         }
 
-         */
-
+        /*
         val goodsNames = arrayOf("Nintendo Switch（OLED款式）白色",
             "PlayStation 5 遊戲主機",
             "PlayStation 5 Digital Edition遊戲主機",
@@ -141,6 +170,7 @@ class homeFragment : Fragment() {
         gridView.setOnItemClickListener { _, _, i, _ ->
             Toast.makeText(context, "${goodsNames[i]}",Toast.LENGTH_SHORT).show()
         }
+        */
     }
 
     companion object {
