e5fe65cc457cf84edd9dc8c356441a68b3a50110
yusheng lin <jacky053696@gmail.com>
2023-06-16 14:41:55 +0800

    checkout init

---
+++ app/src/main/java/com/ncueel/finalproject/MainActivityCheckout.kt
@@ -0,0 +1,11 @@
+package com.ncueel.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityCheckout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_checkout)
+    }
+}
